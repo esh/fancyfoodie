957857dea2a139565059d062789a0f8bbcd3fda6
Edward Chan <esh@mini-esh.(none)>
2010-07-20 00:07:34 +0900

    verify cookie

--- app/utils/facebook.js
+++ app/utils/facebook.js
@@ -1,14 +1,23 @@
 (function() {
-	// todo: VERIFY THE GODDAMN SOURCE - http://developers.facebook.com/docs/authentication/ - md5(payload - sig) = sig
-	var t = request.params["fbs_" + config.facebook_app_id]
+	importPackage(org.apache.commons.codec.digest, java.net)
+
+	var t = URLDecoder.decode(request.params["fbs_" + config.facebook_app_id], "UTF-8")
 	log.info("FB:" + t)
 
 	var data = new Object()
+	var payload = []
 	t.split("&").forEach(function(e) {
 		e = e.split("=")
 		data[e[0]] = e[1]
+		if(e[0] != "sig") {	
+			payload.push(e[0] + "=" + e[1])
+		}
 	})
 
-	log.info(data.toSource())
-	return data
+	if(DigestUtils.md5Hex(payload.sort().join("") + config.facebook_app_secret) == data.sig) {
+		log.info(data.toSource())
+		return data
+	} else {
+		throw "bad facebook creds"
+	}
 })
