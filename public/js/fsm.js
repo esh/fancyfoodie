632b875289a3ee16ce321e8e18375a07ff2d1107
Edward Chan <esh@mini-esh.(none)>
2010-07-22 22:06:26 +0900

    use double click

--- public/js/fsm.js
+++ public/js/fsm.js
@@ -1,51 +1,41 @@
-var FSM = function(states) 
-{
-    this.states = states;
-    this.state = this.states.start;
-    this.state_name = 'start';
-    this.intervalId = null;
+var FSM = function(states) {
+    this.states = states
+    this.state = this.states.start
+    this.state_name = 'start'
 }
 
-FSM.prototype.handle = function(event, data) 
-{
-    var res = null;
-    var target = this.state[event];
-
-    if(target) {
-        if(this.onevent) this.onevent(this, event);
-
-        try {
-            res = target(this, data);
-        } catch(e) {
-            if(this.onexception) {
-                this.onexception(this, e);
-            } else {
-                throw e;
-            }
-        }
-    } else if(this.onunknownevent) {
-        // the target does not exist and they have registered an event for that
-        this.onunknownevent(this, event);
-    }
-    return res
+FSM.prototype.handle = function(event, data) {
+	var res = null
+	var target = this.state[event]
+	if(target) {
+		try {
+			res = target(this, data)
+		} catch(e) {
+			if(this.onexception) {
+				this.onexception(this, e)
+			} else {
+				throw e
+			}
+		}
+	} 
+	return res
 }
 
+FSM.prototype.trans = function(target) {
+	var new_state = this.states[target]
+	if(new_state) {
+		if(this.state.onexit) {
+			this.state.onexit(this, null)
+		}
 
-FSM.prototype.trans = function(target) 
-{
-    var new_state = this.states[target]
-    if(new_state) {
-        if(this.ontrans) this.ontrans(this, target)
-        this.state_name = target
-        this.state = new_state
-	if(this.state.onenter) this.state.onenter(this, null)
-    } else if(this.onunknownstate) {
-        this.onunknownstate(fsm, target)
-    }
+		this.state_name = target
+		this.state = new_state
+		if(this.state.onenter) {
+			this.state.onenter(this, null)
+		}
+	}
 }
 
 FSM.prototype.start = function(event) {
-    this.states.start(this, event);
+	this.states.start(this, event)
 }
-
-
