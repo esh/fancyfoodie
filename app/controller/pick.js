e2b09d3eb059313c528cb9a1832951adefd2c3bf
Edward Chan <esh@mini-esh.(none)>
2010-08-23 22:37:37 +0900

    first cut, needs testing

--- app/controller/pick.js
+++ app/controller/pick.js
@@ -11,6 +11,10 @@
 			var pick = picks.get(key)
 			return ["ok", render("view/pick.jhtml", { key: key, data: pick.data, comments: pick.comments, referees: pick.referees })]
 		},
+		test: function(key) {
+			var pick = picks.get(key)
+			return ["ok", render("view/pick2.jhtml", { key: key, data: pick.data, comments: pick.comments, referees: pick.referees })]
+		},
 		post: function() {
 			var fb = require("model/facebook.js")()
 			var p = JSON.parse(request.content)
@@ -39,9 +43,27 @@
 
 			return ["ok", JSON.stringify(pick), "application/json"]
 		},
-		remove: function(key) {
+		edit: function(key) {
 			var fb = require("model/facebook.js")()
 
+			var p = JSON.parse(request.content)
+			var pick = picks.get(key)
+			if(pick.data.referer_uid == fb.getUID()) {
+				picks.persist({
+					key: key,
+					data: p.data,
+					comments: pick.comments,
+					referees: pick.referees
+				})
+
+
+				return ["ok", "ok"]
+			} else {
+				return ["error", "error"]
+			}
+		},
+		remove: function(key) {
+			var fb = require("model/facebook.js")()
 			var uid = fb.getUID()
 			links.remove(uid, key)
 			queue.add(TaskOptions.Builder.url("/_tasks/remove").param("remove", JSON.stringify({ uid: uid, pick: key })))
