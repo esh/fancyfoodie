2e0e361e01c34ac0582dd3992cb7d60a92f71c21
Edward Chan <esh@mini-esh.(none)>
2010-09-12 23:35:09 +0900

    changed to route based architecture

--- app/init.js
+++ app/init.js
@@ -1,3 +1,20 @@
 var config = require("config.js")
+var pick = require("handler/pick.js")()
+var comments = require("handler/comments.js")()
+var tasks = require("handler/tasks.js")()
 
-httpserver(config, require("utils/dispatcher.js"))
+httpserver(config, require("utils/dispatcher.js")([
+	{ route: /^\/p\/([0-9]+)$/, handler: pick.show },
+	{ route: /^\/r\/([0-9]+)$/, handler: main.yours },	
+	{ route: /^\/pick/show/([0-9]+)$/, handler: pick.show },
+	{ route: /^\/pick/remove/([0-9]+)$/, handler: pick.remove },
+	{ route: /^\/pick/recommend/([0-9]+)$/, handler: pick.recommend },
+	{ route: /^\/pick/post$/, handler: pick.post },
+	{ route: /^\/pick/edit$/, handler: pick.edit },
+	{ route: /^\/comments/post/([0-9]+)$/, handler: comments.post,	
+	{ route: /^\/comments/get/([0-9]+)$/, handler: comments.get,
+	{ route: /^\/_tasks/remove$/, handler: tasks.remove },
+	{ route: /^\/_tasks/addLink$/, handler: tasks.addLink },
+	{ route: /^\/_tasks/addComment$/, handler: tasks.addComments },	
+	{ route: /./, handler: main.show }	
+]))
--- app/routes.js
+++
@@ -1,9 +0,0 @@
-(function(url) {
-	if(url.match(/^\/p\/[0-9]+$/)) {
-		return "/pick/show" + url.slice(2)
-	} else if(url.match(/^\/r\/[0-9]+$/)) {
-		return "/root/yours" + url.slice(2)
-	} else {
-		return url
-	}
-})
