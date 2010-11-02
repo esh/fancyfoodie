8de152ed937d5565fbb7da9aa443e41466b78c98
Edward Chan <esh@mini-esh.(none)>
2010-11-02 20:32:05 +0900

    fix bug and consolidate

--- public/js/geo.js
+++ public/js/geo.js
@@ -7,3 +7,18 @@ var geolocation = (function() {
 		return null
 	}
 })()
+
+function calcDist(lat1, lon1, lat2, lon2) {
+	lat1 = lat1 * Math.PI / 180
+	lat2 = lat2 * Math.PI / 180
+	lon1 = lon1 * Math.PI / 180
+	lon2 = lon2 * Math.PI / 180
+
+	var R = 6371 // km
+	var dLat = lat2-lat1
+	var dLon = lon2-lon1 
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon/2) * Math.sin(dLon/2) 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+	return parseInt(R * c * 100) / 100
+}
+
