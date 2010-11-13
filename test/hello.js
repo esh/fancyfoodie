67650da7198d87fad8a8644c8e5f3acd3a42981a
Edward Chan <esh@mini-esh.(none)>
2010-11-13 09:28:01 +0900

    proof of concept

--- test/hello.js
+++ test/hello.js
@@ -1,6 +1,15 @@
+
 function lookup(place) {
 	return hget("http://maps.google.com/maps?view=text&q=" + escape(place))
 }
 
-var scrape = eval("({" + lookup("hatos bar tokyo").match(/infoWindow:.*basics:/)[0] + "\"\"}})")
-info.log(scrape.toSource())
+var scrape = eval("({" + lookup("hatos bar tokyo").match(/infoWindow:.*basics:/)[0] + "\"\"}})").infoWindow
+
+scrape = {
+	title: scrape.title,
+	address_lines: scrape.addressLines,
+	phones: scrape.phones,
+	url: scrape.hp.actual_url
+}
+
+log.info(scrape.toSource())
