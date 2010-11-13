ed722269fea43236031ef67c13c195ce1b901f7c
Edward Chan <esh@mini-esh.(none)>
2010-11-13 14:07:18 +0900

    proof of concept

--- test/hello.js
+++ test/hello.js
@@ -1,20 +1,33 @@
-function lookup(place) {
-	var uri = new java.net.URI("http", "a.com", "/" + place, null)
+function rgeocode(lat, lng) {
+	return hget("http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=false")
+}
+
+function lookup(name, address) {
+	var uri = new java.net.URI("http", "a.com", "/" + name + " near " + address, null)
 	uri = "http://maps.google.com/maps?view=text&q=" + uri.toASCIIString().slice("http://a.com/".length)
 	return hget(uri)
 }
 
+function populateAddressDetails(name, lat, lng) {
+	var geo = eval("(" + rgeocode(lat, lng) + ")")
+	if(geo.status == "OK") {
+		try {
+			var html = lookup(name, geo.results[0].formatted_address)
+			var scrape = eval("({" + html.match(/infoWindow:.*basics:/)[0] + "\"\"}})").infoWindow
 
-var html = lookup("Jasmine thai near Japan, Tokyo Minato六本木５丁目１８−２２")
-
-//java.lang.System.out.print(html)
-var scrape = eval("({" + html.match(/infoWindow:.*basics:/)[0] + "\"\"}})").infoWindow
-
-scrape = {
-	title: scrape.title,
-	address_lines: scrape.addressLines,
-	phones: scrape.phones,
-	url: scrape.hp.actual_url
+			return {
+				title: scrape.title,
+				address_lines: scrape.addressLines,
+				phones: scrape.phones,
+				url: scrape.hp.actual_url
+			}
+		} catch(e) {
+			log.severe(e)
+			log.severe("by " + name + " latLng: " + lat + ":" + lng)
+		}
+	} else {
+		log.severe("geolookup failed for:" + lat + ":" + lng)
+	}
 }
 
-log.info(scrape.toSource())
+log.info(populateAddressDetails("Jasmine thai", 35.66111479675315, 139.73684287834791).toSource())
